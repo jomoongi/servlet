4809149a69e687aa214fab856118ff6c7412f5cb
jomoongi <jodk1998@Naver.com>
2022-09-26 14:33:29 +0900

    220926 단순하고 실용적인 컨트롤러 -v4

--- src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -1,10 +1,10 @@
-package hello.servlet.web.frontcontroller.v3;
+package hello.servlet.web.frontcontroller.v4;
 
 import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.MyView;
-import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
-import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
-import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,19 +15,19 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*")
-public class FrontControllerServletV3 extends HttpServlet {
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
 
-    private Map<String, ControllerV3> controllerMap = new HashMap<>();
+    private Map<String, ControllerV4> controllerMap = new HashMap<>();
 
     //이게 서버 로딩시 호출되는 이유는
     //수업 내에서는 @ServletComponentScan을 사용하고 있어서 @WebServlet이 붙은 클래스들이 SpringBoot 로드시 빈으로 등록됩니다.
     //그때 빈이 생성될 것이므로 수업 내 코드 기준으로는 @ServletComponentScan으로 인해 @WebServlet이 스캔되고 빈이 등록될 때 생성자가 호출될 것입니다
-    public FrontControllerServletV3() {
-        System.out.println("FrontControllerServletV3.FrontControllerServletV3");
-        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
-        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
-        controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
+    public FrontControllerServletV4() {
+        System.out.println("FrontControllerServletV4.FrontControllerServletV4");
+        controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
     }
 
     @Override
@@ -36,22 +36,20 @@ public class FrontControllerServletV3 extends HttpServlet {
         String requestURI = request.getRequestURI();
 
         //요청 url에 해당하는 인스턴스를 가져온다
-        ControllerV3 controller = controllerMap.get(requestURI);
-        System.out.println(requestURI);
+        //ex) 요청이 들어온 /front-controller/v4/members/new-form 이 이름으로 해당하는 인스턴스를 가져온다
+        ControllerV4 controller = controllerMap.get(requestURI);
         if(controller == null){
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
 
-        //paramMap
-
         Map<String, String> paramMap = createParamMap(request);
-        ModelView mv = controller.process(paramMap);
+        Map<String, Object> model = new HashMap<>(); //추가
+        String viewName = controller.process(paramMap, model);
 
-        String viewName = mv.getViewName();//논리이름 new-form
         MyView view = viewResolver(viewName);
 
-        view.render(mv.getModel(), request,response);
+        view.render(model, request,response);
     }
 
     private static MyView viewResolver(String viewName) {
